f8a0a0a422022851346a66d00622ab10836d733b
HoangHiep05 <119517474+HoangHiep05@users.noreply.github.com>
2023-01-21 17:29:24 +0700

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,30 @@
+// Để thêm bài nhạc mình sẽ them vào list sau
+
+//   {
+//     name: "Tên bài nhạc",
+//     artist: "Nghệ sĩ",
+//     img: "Tên ảnh định dạng .jpg trong project",
+//     src: "Link đường dẫn file nhạc đinh dạng .mp3"
+//   }
+
+let allMusic = [
+  {
+    name: "Kiếp má hồng - speed up",
+    artist: "TLong",
+    img: "kiep-ma-hong",
+    src: "kiep-ma-hong"
+  },
+  {
+    name: "Duyên trời lấy 2",
+    artist: "Chung Thanh Duy",
+    img: "duyen-troi-lay-2",
+    src: "duyen-troi-lay-2"
+  },
+  {
+    name: "100 years love",
+    artist: "NamDuc",
+    img: "100-years-love",
+    src: "100-years-love"
+  },
+
+];
