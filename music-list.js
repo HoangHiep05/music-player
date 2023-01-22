178e60aeec163572175e9c37c5c5f608008b52b4
HoangHiep05 <119517474+HoangHiep05@users.noreply.github.com>
2023-01-22 21:46:03 +0700

    Update music-list.js

--- music-list.js
+++ music-list.js
@@ -1,12 +1,3 @@
-// Để thêm bài nhạc mình sẽ them vào list sau
-
-//   {
-//     name: "Tên bài nhạc",
-//     artist: "Nghệ sĩ",
-//     img: "Tên ảnh định dạng .jpg trong project",
-//     src: "Link đường dẫn file nhạc đinh dạng .mp3"
-//   }
-
 let allMusic = [
   {
     name: "Kiếp má hồng - speed up",
@@ -27,7 +18,7 @@ let allMusic = [
     src: "anh-sao-va-bau-troi"
   },
   {
-    name: "Tình ca tình ca",
+    name: "Tình ca tình ca - speed up",
     artist: "Kis",
     img: "tinh-ca-tinh-ca",
     src: "tinh-ca-tinh-ca"
