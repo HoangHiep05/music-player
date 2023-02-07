8c811cd9d4414f13695626c087360b836205bbbe
HoangHiep05 <119517474+HoangHiep05@users.noreply.github.com>
2023-02-07 21:08:31 +0700

    Add files via upload

--- music.js
+++ music.js
@@ -23,6 +23,12 @@
       isRepeat: false,
       songs: [
         {
+          name: 'Mashup Crush - speed up',
+          singer: '##',
+          path: './music/mashup-crush.mp3',
+          image: './music/mashup-crush.jpg',
+        },
+        {
           name: 'Cherry Love - speed up',
           singer: 'Híu',
           path: './music/cherry-love.mp3',
@@ -40,12 +46,12 @@
           path: './music/4-mua-thuong-em.mp3',
           image: './music/4-mua-thuong-em.jpg',
         },
-//         {
-//           name: '2AM - speed up',
-//           singer: 'JustaTee x BigDaddy x Enderlazer',
-//           path: './music/2am.mp3',
-//           image: './music/2am.jpg',
-//         },
+        // {
+        //   name: '2AM - speed up',
+        //   singer: 'JustaTee x BigDaddy x Enderlazer',
+        //   path: './music/2am.mp3',
+        //   image: './music/2am.jpg',
+        // },
         {
           name: 'Anh đâu có hay - speed up',
           singer: 'LEMESE X AUGUST',
